244f292870279df77b68ffbfb7f94977057549c2
Michael Mols <mgfmols@gmail.com>
2025-07-30 22:24:26 +0200

    Adjusted Context file

--- Editor/Template/TemplateContext.cs
+++ Editor/Template/TemplateContext.cs
@@ -1,7 +1,11 @@
-﻿namespace State_Machine_Creator.Editor.Template
+﻿using System;
+using UnityEngine;
+
+namespace State_Machine_Creator.Editor.Template
 {
+    [Serializable]
     public class TemplateContext
     {
-        
+        [field: SerializeField] public string SomeField { get; set; }
     }
 }
